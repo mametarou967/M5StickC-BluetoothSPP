ccd2cdfc4da6f28f2496f246cb61429fc60dc644
mametarou967 <mametarou967@gmail.com>
2023-12-30 22:38:02 +0900

    最低限の通信

--- Central/Central.ino/Central.ino.ino
+++ Central/Central.ino/Central.ino.ino
@@ -15,8 +15,10 @@
  * use ESP_SPP_SEC_NONE or ESP_SPP_SEC_ENCRYPT|ESP_SPP_SEC_AUTHENTICATE in connect() if remote side has Authentication: False
  */
 
+#include <stdarg.h>
 #include <map>
 #include <BluetoothSerial.h>
+#include <M5Unified.h> 
 
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
@@ -35,8 +37,33 @@ esp_spp_role_t role=ESP_SPP_ROLE_SLAVE; // or ESP_SPP_ROLE_MASTER
 
 // std::map<BTAddress, BTAdvertisedDeviceSet> btDeviceList;
 
+void printBoth(bool ln,const char* format, ...) {
+  va_list args;
+  va_start(args, format);
+
+  // フォーマットされた文字列を格納するためのバッファを作成
+  char buffer[256]; // 適切なサイズを設定してください
+  vsnprintf(buffer, sizeof(buffer), format, args);
+
+  if(ln) println(buffer);
+  else print(buffer);
+
+  va_end(args);
+}
+
+void println(const char* str) {
+  Serial.println(str);         // シリアルモニターにHello World!!と1行表示
+  M5.Display.println(str);       // 画面に1行表示
+}
+
+void print(const char* str){
+  Serial.print(str);         // シリアルモニターにHello World!!と1行表示
+  M5.Display.print(str);       // 画面に1行表示
+}
+
 void setup() {
-  Serial.begin(115200);
+  auto cfg = M5.config();       // M5Stack初期設定用の構造体を代入
+  M5.begin(cfg);                // M5デバイスの初期化
   if(! SerialBT.begin("ESP32test", true) ) {
     Serial.println("========== serialBT failed!");
     abort();
